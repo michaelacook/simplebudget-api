278c2b201d51edab4e2ff162547c6e0d904cbf43
Michael A. Cook <mcook0775@gmail.com>
2020-12-08 17:36:14 -0500

    Add associations

--- models/budget.js
+++ models/budget.js
@@ -2,13 +2,13 @@
 const { Model } = require("sequelize")
 module.exports = (sequelize, DataTypes) => {
   class Budget extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate({ Category, User }) {
+      Budget.hasMany(Category, {
+        foreignKey: "budgetId",
+      })
+      Budget.belongsTo(User, {
+        foreignKey: "userId",
+      })
     }
   }
   Budget.init(
