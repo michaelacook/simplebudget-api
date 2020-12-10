b79147700a42398da4e1507958bc18575f878f6a
Michael A. Cook <mcook0775@gmail.com>
2020-12-09 21:10:57 -0500

    Fix getUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,7 @@ module.exports = {
    */
   getUser: async (req, res, next) => {
     try {
-      const id = req.params.id
+      const email = req.user.email
       const budget = req.query.budget
       const user = await userService.getUser(id, budget ? true : null)
       return res.status(200).json(user)
