7aeb1219a42f2e1f6b15b92955e8038427126dc5
Michael A. Cook <mcook0775@gmail.com>
2020-12-09 21:14:45 -0500

    Fix usersRoute middleware call  base path

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
-app.use("/users", usersRouter)
+app.use("/user", usersRouter)
 app.use("/budgets", budgetsRouter)
 app.use("/bills", billsRouter)
 
