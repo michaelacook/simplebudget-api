54486a02550c8278af746b885c42c7a20c8255a0
Michael A. Cook <mcook0775@gmail.com>
2020-12-09 20:46:22 -0500

    Finish /users routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get("/:id", (req, res, next) => {
   userController.getUser(req, res, next)
 })
 
+router.post("/create", (req, res, next) => {
+  userController.postUser(req, res, next)
+})
+
+router.put("/:id/update", (req, res, next) => {
+  userController.putUser(req, res, next)
+})
+
+router.delete("/:id", (req, res, next) => {
+  userController.deleteUser(req, res, next)
+})
+
 module.exports = router
