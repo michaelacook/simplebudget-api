df33dbc46d624e3387d8468a1cb45d2a6d643786
Michael A. Cook <mcook0775@gmail.com>
2020-12-14 21:21:59 -0500

    Changes

--- middleware/validation/signup.js
+++ middleware/validation/signup.js
@@ -3,20 +3,30 @@ const { body } = require("express-validator")
 module.exports = [
   body("firstName")
     .isAlpha()
-    .withMessage(
-      "Please provide a name that contains only alphabetical characters"
-    ),
+    .withMessage("First name should contain only alphabetical characters."),
+  body("lastName")
+    .isAlpha()
+    .withMessage("Last name should contain only alphabetical characters."),
   body("email").isEmail().withMessage("Please provide a valid email."),
   body("password")
     .isAlphanumeric()
     .withMessage("Password can only contain numbers and letters."),
-  body("confirmPassword")
-    .equals(body("password"))
-    .withMessage("The passwords you have provided do not match."),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Password confirmation does not match password.")
+    }
+    return true
+  }),
   body("netSalary")
     .isNumeric()
     .withMessage("Net salary must contain only numbers"),
   body("netMonthlyIncome")
     .isNumeric()
-    .withMessage("Net monthly income must contain only numbers."),
+    .withMessage("Net monthly income must contain only numbers.")
+    .custom((value, { req }) => {
+      if (value >= req.body.netSalary) {
+        throw new Error("Monthly income cannot be greater or equal to salary.")
+      }
+      return true
+    }),
 ]
