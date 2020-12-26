0993211ba90cb0535b91c108ed1cb38b8901fa63
Michael A. Cook <mcook0775@gmail.com>
2020-12-26 13:31:44 -0500

    Add comments

--- controllers/expenditureController.js
+++ controllers/expenditureController.js
@@ -1,6 +1,12 @@
 const { expenditureService } = require("../services/expenditureService")
 
 module.exports = {
+  /**
+   * Handle route /expenditures/new POST
+   * @param {Object} req - HTTP request
+   * @param {Object} res - HTTP response
+   * @param {Func} next - next middleware call
+   */
   postExpenditure: async (req, res, next) => {
     try {
       const expenditures = req.body.expenditures
