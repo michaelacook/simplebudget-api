b3a4b3dac096322306b4b75eeab3ca6bcaca1832
Michael A. Cook <mcook0775@gmail.com>
2020-12-26 22:56:53 -0500

    Add expenditure routes

--- routes/expenditures.js
+++ routes/expenditures.js
@@ -3,8 +3,20 @@ const router = express.Router()
 const expenditureController = require("../controllers/expenditureController")
 const authorization = require("../middleware/authorization")()
 
+router.get("/:id?", authorization, (req, res, next) =>
+  expenditureController.getExpenditure(req, res, next)
+)
+
 router.post("/new", authorization, (req, res, next) =>
   expenditureController.postExpenditure(req, res, next)
+)
+
+router.put("/:id", authorization, (req, res, next) =>
+  expenditureController.putExpenditure(req, res, next)
+)
+
+router.delete("/:id", authorization, (req, res, next) =>
+  expenditureController.deleteExpenditure(req, res, next)
 )
 
 module.exports = router
