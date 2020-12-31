22b7184bfa7f3f916ad47aa7f43f7812b220add9
Michael A. Cook <mcook0775@gmail.com>
2020-12-30 21:39:45 -0500

    Add billController

---
+++ controllers/billController.js
@@ -0,0 +1,83 @@
+const billService = require("../services/billService")
+
+module.exports = {
+  /**
+   * Handle route /bill/:id GET
+   * @param {Object} req - HTTP request
+   * @param {Object} res - HTTP response
+   * @param {Func} next - next middleware call
+   */
+  getBill: async (req, res, next) => {
+    try {
+      const id = req.params.id
+      const bill = await billService.getBill(id)
+      return res.status(200).json(bill)
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  /**
+   * Handle route /bill GET
+   * @param {Object} req - HTTP request
+   * @param {Object} res - HTTP response
+   * @param {Func} next - next middleware call
+   */
+  getBills: async (req, res, next) => {
+    try {
+      const userId = req.user.id
+      const bills = await billService.getAllBills(userId)
+      return res.status(200).json(bills)
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  /**
+   * Handle route /bill POST
+   * @param {Object} req - HTTP request
+   * @param {Object} res - HTTP response
+   * @param {Func} next - next middleware call
+   */
+  postBill: async (req, res, next) => {
+    try {
+      const userId = req.user.id
+      const bill = await billService.addBill(userId, req.body)
+      return res.status(201).json(bill)
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  /**
+   * Handle route /bill/:id PUT
+   * @param {Object} req - HTTP request
+   * @param {Object} res - HTTP response
+   * @param {Func} next - next middleware call
+   */
+  putBill: async (req, res, next) => {
+    try {
+      const id = req.params.id
+      await billService.updateBill(id, req.body)
+      return res.status(204).end()
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  /**
+   * Handle route /bill/:id DELETE
+   * @param {Object} req - HTTP request
+   * @param {Object} res - HTTP response
+   * @param {Func} next - next middleware call
+   */
+  deleteBill: async (req, res, next) => {
+    try {
+      const id = req.params.id
+      await billService.deleteBill(id)
+      return res.status(204).end()
+    } catch (error) {
+      next(error)
+    }
+  },
+}
