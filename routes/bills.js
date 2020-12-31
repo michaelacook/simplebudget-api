556de2323e17824a580b6f93052b9d13993a6a58
Michael A. Cook <mcook0775@gmail.com>
2020-12-30 21:53:55 -0500

    Fill in bill router

--- routes/bills.js
+++ routes/bills.js
@@ -1,4 +1,26 @@
 const express = require("express")
 const router = express.Router()
+const billController = require("../controllers/billController")
+const authorization = require("../middleware/authorization")
+
+router.get("/:id", authorization, (req, res, next) =>
+  billController.getBill(req, res, next)
+)
+
+router.get("/", authorization, (req, res, next) =>
+  billController.getBills(req, res, next)
+)
+
+router.post("/", authorization, (req, res, next) =>
+  billController.postBill(req, res, next)
+)
+
+router.put("/:id", authorization, (req, res, next) =>
+  billController.putBill(req, res, next)
+)
+
+router.delete("/:id", authorization, (req, res, next) =>
+  billController.deleteBill(req, res, next)
+)
 
 module.exports = router
