7ef74f8884be58380fb52fd9b543ef499e32f9ee
Michael A. Cook <mcook0775@gmail.com>
2021-01-31 13:16:24 -0500

    Add length check to first name, last name and password in validation middleware

--- middleware/validation/signup.js
+++ middleware/validation/signup.js
@@ -3,14 +3,29 @@ const { body } = require("express-validator")
 module.exports = [
   body("firstName")
     .isAlpha()
-    .withMessage("First name should contain only alphabetical characters."),
+    .withMessage("First name should contain only alphabetical characters.")
+    .isLength({
+      min: 1,
+      max: undefined,
+    })
+    .withMessage("First name cannot be of 0 length."),
   body("lastName")
     .isAlpha()
-    .withMessage("Last name should contain only alphabetical characters."),
+    .withMessage("Last name should contain only alphabetical characters.")
+    .isLength({
+      min: 1,
+      max: undefined,
+    })
+    .withMessage("Last name cannot be of 0 length."),
   body("email").isEmail().withMessage("Please provide a valid email."),
   body("password")
     .isAlphanumeric()
-    .withMessage("Password can only contain numbers and letters."),
+    .withMessage("Password can only contain numbers and letters.")
+    .isLength({
+      min: 5,
+      max: undefined,
+    })
+    .withMessage("Password must be at least 5 characters long."),
   body("confirmPassword").custom((value, { req }) => {
     if (value !== req.body.password) {
       throw new Error("Password confirmation does not match password.")
